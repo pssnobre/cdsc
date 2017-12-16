bc2f7d48fbf674b8ea2ed110194a48499f541509
Unknown <psnobre@hotmail.com>
2017-12-16 17:32:04 -0300

    Commit 2.

--- CDSC/App_Start/RouteConfig.cs
+++ CDSC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CDSC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "IdentificacaoCrianca", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
